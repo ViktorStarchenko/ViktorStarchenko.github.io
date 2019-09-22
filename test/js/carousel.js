78c0a3e48a7148e049f67719ea367aaf06dc6d7b
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-09-22 17:55:09 +0300

    massage2

--- test/js/carousel.js
+++ test/js/carousel.js
@@ -16,8 +16,8 @@ $('.related__slider-initiate').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
@@ -26,8 +26,8 @@ $('.related__slider-initiate').slick({
       breakpoint: 600,
       settings: {
         arrows: false,
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
