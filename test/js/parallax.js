86d9e5236aa092f79af0ceba846acfe00d38c837
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-10-07 15:42:20 +0300

    tur42 test menu

---
+++ test/js/parallax.js
@@ -0,0 +1,40 @@
+new universalParallax().init({
+        speed: 9.0
+    });
+
+
+
+// // ****************NON MOBILE PARALLAX******************
+
+// 	// INDEX PAGE:
+
+// 	// whyus__banner-img
+// 	$('.why-us__banner-img').parallax({imageSrc: 'img/new3.png'});
+
+// 	// offerbanner__img
+// 	$('.offer-banner__img').parallax({imageSrc: 'img/new4.png'});
+
+
+
+
+
+// 	// parallax for brand-reasons block
+// 	$('.brand-reasons__banner-img').parallax({imageSrc: 'img/single/brand-reason-banner.png'});
+
+
+
+// 	// hero parallax:
+// 	$('.index-hero-banner').parallax({imageSrc: 'img/new.png'});
+// 	$('.abb-hero-banner').parallax({imageSrc: 'img/single/hero.png'});
+// 	$('.garret-hero-banner').parallax({imageSrc: 'img/garret/hero.png'});
+// 	$('.ihi-hero-banner').parallax({imageSrc: 'img/ihi/hero.png'});
+// 	$('.caterpillar-hero-banner').parallax({imageSrc: 'img/cat/hero.png'});
+// 	$('.holset-hero-banner').parallax({imageSrc: 'img/holset/hero.png'});
+// 	$('.perkins-hero-banner').parallax({imageSrc: 'img/perkins/hero.png'});
+
+
+
+// 	// ihi banner-1
+// 	$('.ihi-banner__img').parallax({imageSrc: 'img/ihi/img-banner.jpg'});
+
+
