9fb0dde7361f0f232ad0cd98e3521d5661f06ac5
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-11-06 13:26:40 +0200

    турбо. тест письма

---
+++ dev/test.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+$name = $_POST['callback-modal-name'];
+$phone = $_POST['callback-modal-phone'];
+
+echo 'name: ';
+echo $name;
+echo '<hr>';
+echo 'phone: ';
+echo $phone;
+echo '<hr>';
