e6170cb70749e19d5dfc85919d7735e5c2696b7c
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-11-29 02:11:24 +0200

    green 10

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
 		$('.shadow-screen').toggleClass('active')
 		$('body').toggleClass('hidden')
 	})
+
+
+
+	$(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 0) {
+             $('#header .navbar').addClass('scrolled');
+         }
+         else {
+             $('#header .navbar').removeClass('scrolled');
+         }
+    });
 })
