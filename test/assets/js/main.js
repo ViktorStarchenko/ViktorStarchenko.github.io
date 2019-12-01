63e364dc202794bb85de6ce86bce75524c97cf23
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-12-01 21:12:43 +0200

    gallery добавил ссылки

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -26,3 +26,31 @@ var msnry = new Masonry( container, {
   itemSelector: '.some2__item',
   gutter: 10
 });
+
+$(document).ready(function(){
+	
+	$('.some3__item-text').each(function(i,elem) {
+		// console.log($(".some3__item-text").siblings())
+		blockWidth = $(this).siblings();
+		parentWidth = $(".some3__item-text").parent()
+		console.log(parentWidth)
+		console.log('Ширина родителя')
+		console.log(parentWidth[i].clientWidth)
+		parentWidth = parentWidth[i].clientWidth
+		blockWidth = blockWidth[0].clientWidth
+		result = (parseInt(parentWidth) - parseInt(blockWidth))
+		console.log('Ширина фотки')
+		console.log(blockWidth)
+		$(this).width(blockWidth)
+		$(this).css('left', result/2)
+		console.log('отступ')
+		console.log(result/2)
+		console.log('')
+	// if ($(this).hasClass("stop")) {
+	// 	alert("Остановлено на " + i + "-м пункте списка.");
+	// 	return false;
+	// } else {
+	// 	alert(i + ': ' + $(elem).text());
+	// }
+});
+})
