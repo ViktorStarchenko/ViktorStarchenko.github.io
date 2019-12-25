2bb8490663114f97bb7a5f45f8f3cfad4f731f4c
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2019-12-25 15:20:35 +0200

    зеленые зоны

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -125,3 +125,10 @@ $(document).ready(function(){
         }]
     });
 });
+
+
+
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
