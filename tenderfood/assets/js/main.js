e215ac27de6105867a452a36762f1a48cc5d3801
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2020-01-31 01:03:25 +0200

    биржа свинины. Первый коммит

---
+++ tenderfood/assets/js/main.js
@@ -0,0 +1,47 @@
+
+// БЛОК БАННЕРОВ
+window.onload=function(){ 
+  if(window.innerWidth > 520){
+  // $('.banners-slider__wrapper').slick('unslick')
+  }
+}
+
+window.onload=function(){if(window.innerWidth <= 520){ // ДОБАВЛЯЕМ СЛАЙДЕР НА МОБИЛЬНЫХ ЭКРАНАХ
+  $('.banners-slider__wrapper').slick({
+    // slidesToScroll: 1,
+    centerMode: true,
+    // centerPadding: '20px',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 1
+  });
+}
+}
+
+
+// Show mobile menu
+
+$(document).ready(function() {
+  $('.navbar__mobile-menu-button').on('click', function() {
+    $('.mobile-menu-wrap').addClass('show');
+    $('.screen-shadow').addClass('show');
+    $('body').addClass('hidden');
+
+    // $('.mobile-menu-wrap .close-button').on('click', function() {
+    //   $('.mobile-menu-wrap').removeClass('show');
+    //   $('.screen-shadow').removeClass('show');
+    //   $('body').removeClass('hidden');
+    // })
+
+    $('.screen-shadow.show, .mobile-menu-wrap .close-button').on('click', function() {
+      $('.mobile-menu-wrap').removeClass('show');
+      $('.screen-shadow').removeClass('show');
+      $('body').removeClass('hidden');
+    })
+
+  })
+})
+
+
