1ff877233538e1ee40e755caa66cb39bc723aabe
ViktorStarchenko <32950174+ViktorStarchenko@users.noreply.github.com>
2020-08-26 11:34:55 +0300

    Eschools. new eschhol.info 9

--- Eschool/assets/js/main.js
+++ Eschool/assets/js/main.js
@@ -272,3 +272,17 @@ $(document).ready(function(){
 
 // var preg = $(this).val().replace(/[^\d.]/ig, '');
 // $(this).val(parseFloat(preg));
+
+///////////////////////////////////////////////// SMOOTH SCROLL
+// Smooth scroll
+$(document).ready(function(){
+	$('.crane').on("click", function (event) {
+	        event.preventDefault();
+	        //забираем идентификатор бока с атрибута href
+	        var id  = $(this).attr('href'),
+	        //узнаем высоту от начала страницы до блока на который ссылается якорь
+	            top = $(id).offset().top;
+	        //анимируем переход на расстояние - top за 1500 мс
+	        $('body,html').animate({scrollTop: top-100}, 1500);
+	    });
+})
