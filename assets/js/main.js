0c83ffa17a1c7b9f2c5a6b67bb767d149dc9138b
Viktor Starchenko <viktor.s@overdose.digital>
2020-10-01 16:25:28 +0300

    fantails init commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////// HEADER FIXED /////////////////////////////////////////////////////////////
+
+window.onload = function () {
+    var header = $(".header_fixed").offset().top;
+    var sticky = header.offsetTop;
+    // console.log(header);
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function headerFixed() {
+        if (window.pageYOffset > 0) {
+            $(".header_fixed").addClass("fixed");
+            $("main").addClass("header-padding");
+        } else {
+            $(".header_fixed").removeClass("fixed");
+            $("main").removeClass("header-padding");
+        }
+    }
+
+    window.onscroll = function () {
+        headerFixed();
+    };
+};
